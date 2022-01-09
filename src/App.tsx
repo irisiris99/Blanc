132a2cc1b276b231abad100be9d52f2b8bd2be38
irisiris99 <cjg03117@gmail.com>
2022-01-09 22:33:46 +0900

    0109d

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react';
 import './App.css';
 import { useSelector } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Link, HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Floor from './pages/Floor';
 import Villa from './pages/Villa';
