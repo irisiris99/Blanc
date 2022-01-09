15524c172e40feeecc43c9c20929499166456d71
irisiris99 <cjg03117@gmail.com>
2022-01-09 18:31:22 +0900

    d

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ let store = createStore(reducer)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
